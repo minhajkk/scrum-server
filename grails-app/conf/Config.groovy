c478a45b1c91913e072c26c20774084aaa63ce93
minhajkk <minhaj.kk@gmail.com>
2014-06-16 08:53:49 +0400

    Enabled json pretty print

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -11,7 +11,7 @@
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
 
-grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
+grails.project.groupId = com.scrum // change this to alter the default package name and Maven publishing destination
 
 // The ACCEPT header will not be used for content negotiation for user agents containing the following strings (defaults to the 4 major rendering engines)
 grails.mime.disable.accept.header.userAgents = ['Gecko', 'WebKit', 'Presto', 'Trident']
@@ -155,3 +155,4 @@ grails.plugin.springsecurity.rest.token.storage.gorm.tokenDomainClassName='com.s
 grails.plugin.springsecurity.logout.postOnly = false
 grails.plugin.springsecurity.auth.loginFormUrl = "/auth/login"
 grails.plugin.springsecurity.failureHandler.defaultFailureUrl = "/auth/login"
+grails.converters.default.pretty.print=true
